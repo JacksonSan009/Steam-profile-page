cfb4c9cc5ef9f0030f70916b501786c8198f5919
Jackson Arango <jacksonsan009@hotmail.com>
2025-01-02 17:36:45 -0500

    change path

--- Steam-Profile-Page/ClientApp/src/app/app.module.ts
+++ Steam-Profile-Page/ClientApp/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,13 +17,17 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-    ])], providers: [{ provide: 'BASE_URL', useFactory: getBaseUrl }, provideHttpClient(withInterceptorsFromDi())
-    ]
+      { path: "", component: HomeComponent, pathMatch: "full" },
+      { path: "**", component: HomeComponent },
+    ]),
+  ],
+  providers: [
+    { provide: "BASE_URL", useFactory: getBaseUrl },
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
 })
-
 export class AppModule { }
 
 export function getBaseUrl() {
-  return document.getElementsByTagName('base')[0].href;
+  return document.getElementsByTagName("base")[0].href;
 }
